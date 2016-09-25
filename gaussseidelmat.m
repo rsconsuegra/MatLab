fcb560b5895e085e8f936c17c4a71aff2eb8dd4e
Randy Steven Consuegra <rsconsuegra95@gmail.com>
2016-09-25 02:48:17 -0500

    Update gaussseidelmat.m

--- gaussseidelmat.m
+++ gaussseidelmat.m
@@ -14,19 +14,18 @@ end
 if(sw==0)
 	b=input('Digite Vector de terminos independientes b=');
 	x0=input('Digite vector semilla x0=');
-    
-    La=tril(A);
-    U=triu(A)-diag(diag(A));
-    T=-La\U;
-    C=La\b;
-
-    error=1;
-    tol=1e-8;
-    while(error>=tol)
-        xn=T*x0+C;
-        %%Criterio de parada: Normal vectorial. 
-        error=norm(xn-x0);
-        x0=xn;
-    end
-    x0
+	
+	La=tril(A);
+	U=triu(A)-diag(diag(A));
+	T=-La\U;
+	C=La\b;
+	error=1;
+	tol=1e-8;
+	while(error>=tol)
+        	xn=T*x0+C;
+        	%%Criterio de parada: Normal vectorial. 
+        	error=norm(xn-x0);
+		x0=xn;
+	end
+    	x0
 end
